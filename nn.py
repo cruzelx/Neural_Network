30a538dd83399568092597069b67aae31bd60bfb
cruzelx <cruze.alex@gmail.com>
2018-06-11 23:40:26 +0545

    A single Perceptron

---
+++ nn.py
@@ -0,0 +1,20 @@
+import random,math
+class Perceptron:
+    def __init__(self,weights):
+        self.weights = weights
+        for i in range(len(self.weights)):
+           self.weights[i] = random.randint(-1,1)
+
+    def signn(self,n):
+        print(1/(1+math.exp(-n)))
+
+  
+    def guess(self,inputs):
+        sum=0
+        for i in range(len(self.weights)):
+            sum += self.weights[i]*inputs[i]
+        self.signn(sum)
+    
+
+X = Perceptron([1,2,3])
+X.guess([1,-1,2])
